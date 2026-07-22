bc9f50b38b8f8c45a462ca9362a9fb540f6ca365
michael <nuns-pastes-0s@icloud.com>
2026-07-22 11:50:53 -0700

    added notes to "hello world"

--- actix_web/basics/hello_world/src/main.rs
+++ actix_web/basics/hello_world/src/main.rs
@@ -1,16 +1,28 @@
 use actix_web::{web, App, get, post, Responder, HttpResponse, HttpServer};
 
-#[get("/")]
+#[get("/")] // handles the HTTP GET request for URL path "/"
 async fn hello() -> impl Responder {
+    // Create an instance of HttpResponse with a status code 200 OK
+    // and attaches the string "Hello world!" as the body content of this response.
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
+#[post("/echo")] // handles the HTTP POST request for URL path "/echo"
 async fn echo(req_body: String) -> impl Responder {
+    // Create an instance of HttpResponse with a status code 200 OK
+    // and attaches the original request body as the body content of this response.
     HttpResponse::Ok().body(req_body)
 }
+/*
+To test this code, enter the following into a terminal using the cURL CLI
+```
+curl -X POST http://127.0.0.1:8080/echo --data "Hello from cURL"
+```
+ */
 
 async fn manual_hello() -> impl Responder {
+    // Create an instance of HttpResponse with a status code 200 OK
+    // and attaches the string "hey there!" as the body content of this response.
     HttpResponse::Ok().body("Hey there!")
 }
 
@@ -18,10 +30,10 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(hello) // register HTTP service
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/hey", web::get().to(manual_hello)) // configure route path
     })
-        .bind("127.0.0.1:8080")?
+        .bind("127.0.0.1:8080")? // binds to socket address and listens
         .run().await
 }
